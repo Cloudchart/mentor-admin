8da1a9d791bcf28e4a40aca5f5461902473d0f51
Andrey Peresleguine <peresleguine@gmail.com>
2016-07-10 20:09:51 +0200

    added app bar nav

--- frontend/javascripts/components/Header.js
+++ frontend/javascripts/components/Header.js
@@ -1,16 +1,40 @@
 import React, { Component, PropTypes } from 'react'
+
 import AppBar from 'material-ui/AppBar'
+import IconMenu from 'material-ui/IconMenu'
+import IconButton from 'material-ui/IconButton'
+import MenuItem from 'material-ui/MenuItem'
+import MenuIcon from 'material-ui/svg-icons/navigation/menu'
 
 
 class Header extends Component {
 
+  // handlers
+  //
+  handleMenuItemClick(path, event) {
+    window.location.href = path
+  }
+
+  // renderers
+  //
   render() {
     const title = this.props.title ?
       `${process.env.APP_NAME} – ${this.props.title}` :
       process.env.APP_NAME
 
     return (
-      <AppBar title={ title } />
+      <AppBar
+        title={ title }
+        iconElementLeft={
+          <IconMenu
+            iconButtonElement={
+              <IconButton><MenuIcon color="#FFFFFF" /></IconButton>
+            }
+          >
+            <MenuItem primaryText="Surveys" onTouchTap={ this.handleMenuItemClick.bind(this, '/surveys') } />
+          </IconMenu>
+        }
+      />
     )
   }
 
